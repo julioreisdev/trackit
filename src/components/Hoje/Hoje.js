6acf130a6cca1089e807b8919d69662cbe953f6f
julioreisdev <juliosteam0352@gmail.com>
2022-05-25 21:11:00 -0300

    Adding footter in to Menu component

--- src/components/Hoje/Hoje.js
+++ src/components/Hoje/Hoje.js
@@ -1,9 +1,16 @@
 import Menu from "../Menu/Menu";
+import styled from "styled-components";
 
-export default function Hoje () {
+export default function Hoje() {
   return (
-    <>
+    <Container>
       <Menu />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color:  #E5E5E5;
+`;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,36 @@
 import styled from "styled-components";
 import { useContext } from "react";
 import DadosUser from "../Context/DadosUser";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-  const { imgUser, setImgUser } = useContext(DadosUser);
-  const { nomeUser, setNomeUser } = useContext(DadosUser);
+  const { imgUser } = useContext(DadosUser);
+  const { nomeUser } = useContext(DadosUser);
 
   return (
     <Container>
-      <h1>TrackIt</h1>
-      <img src={imgUser} alt={nomeUser} />
+      <Header>
+        <h1>TrackIt</h1>
+        <img src={imgUser} alt={nomeUser} />
+      </Header>
+      <Footer>
+        <Link className="link" to="/habitos">
+          <p>Hábitos</p>
+        </Link>
+        <Link className="link" to="/hoje">
+          <div>Hoje</div>
+        </Link>
+        <Link className="link" to="/historio">
+          <p>Histórico</p>
+        </Link>
+      </Footer>
     </Container>
   );
 }
 
-const Container = styled.header`
+const Container = styled.div``;
+
+const Header = styled.header`
   background-color: #126ba5;
   width: 100%;
   height: 70px;
@@ -24,6 +40,8 @@ const Container = styled.header`
   padding: 1rem;
   box-shadow: 0 1px 5px gray;
   font-family: "Playball", cursive;
+  position: fixed;
+  top: 0;
 
   h1 {
     font-size: 2.5rem;
@@ -34,5 +52,37 @@ const Container = styled.header`
     width: 40px;
     height: 40px;
     border-radius: 50%;
+  }
+`;
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  box-shadow: 0 -1px 5px gainsboro;
+
+  p {
+    color: #52b6ff;
+  }
+
+  div {
+    font-weight: bold;
+    width: 90px;
+    height: 90px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: #52b6ff;
+    position: absolute;
+    bottom: 0.5rem;
+    right: calc(50% - 45px);
   }
 `;
